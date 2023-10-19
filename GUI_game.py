6c8bf236cee7474420e87da873cc2b846b6682b9
id-hkj <83733473+id-hkj@users.noreply.github.com>
2023-10-19 19:48:54 +0100

    Update GUI_game.py
    Will now work with any No. of players/teams that are listed in the rules for my physical copy of the sequence board game (2 teams/3 teams, up to 12 total players)

--- GUI_game.py
+++ GUI_game.py
@@ -19,8 +19,7 @@ class GameScreen:
         self.card_change_stage = 0
         self.card_x = []
         self.card_y = []
-        self.RedNames = []
-        self.BlueNames = []
+        self.player_names = {'r': "", 'g': "", 'b': ""}
         self.reply = 'ping'
         self.top_txt = 'Waiting for '
         self.turn = False
@@ -35,10 +34,9 @@ class GameScreen:
         self.card_select = 'xy'
         self.clicked = ''
         self.card_selected = False
-        self.run = True
         self.winCards = []
         self.highlighter = []
-        self.detail = {'AI': 0, 'US': 0}
+        self.detail = {'AI': 0, 'US': 0, 'GR': 0}
         self.sequence_no = {}
         self.winner = ''
         self.Font = pygame.font.Font(None, 40)
@@ -51,12 +49,16 @@ class GameScreen:
         self.BoardCards = pygame.image.load(os.path.join('Sequence!!!.png')).convert_alpha()
         self.USChip = pygame.image.load(os.path.join('Blue.png')).convert_alpha()
         self.AIChip = pygame.image.load(os.path.join('Red.png')).convert_alpha()
+        self.GRChip = pygame.image.load(os.path.join('Green.png')).convert_alpha()
         self.USChip = pygame.transform.smoothscale(self.USChip, (int(50 // self.zr), int(50 // self.zr)))
         self.AIChip = pygame.transform.smoothscale(self.AIChip, (int(50 // self.zr), int(50 // self.zr)))
+        self.GRChip = pygame.transform.smoothscale(self.GRChip, (int(50 // self.zr), int(50 // self.zr)))
         self.mini_USChip = pygame.transform.smoothscale(self.USChip, (int(15 // self.zr), int(15 // self.zr)))
         self.mini_AIChip = pygame.transform.smoothscale(self.AIChip, (int(15 // self.zr), int(15 // self.zr)))
+        self.mini_GRChip = pygame.transform.smoothscale(self.GRChip, (int(15 // self.zr), int(15 // self.zr)))
         self.BoardCards = pygame.transform.smoothscale(self.BoardCards, (int(960 // self.zr), int(689 // self.zr)))
         self.cardIMGs = {}
+        self.legend_prep = []
         self.data = None
         for cardNo in range(52):
             self.cardIMGs[str(self.cards[cardNo])] = pygame.image.load(
@@ -97,6 +99,8 @@ class GameScreen:
         self.sequence_no = {'US': 0, 'AI': 0}
         self.winCards = []
         names = ['US', 'AI']
+        if self.player_names['g']:
+            names.append('GR')
         check_boards = {1: gameBoard, 2: self.transpose(self.shift(gameBoard)), 3: self.transpose(gameBoard),
                         4: self.transpose(self.shift(reversed(gameBoard)))}
         # USER CHECK
@@ -106,7 +110,7 @@ class GameScreen:
                 counter = 0
                 for i in range(len(board_test)):
                     for j in range(len(board_test[i])):
-                        if board_test[i][j][-2:] == name or board_test[i][j][-2:] == 'xx':
+                        if board_test[i][j][-2:] == name or board_test[i][j] == 'xx':
                             counter += 1
                             if counter >= 5:
                                 self.sequence_no[name] = 1
@@ -121,6 +125,14 @@ class GameScreen:
                         else:
                             counter = 0
                     counter = 0
+            if 'GR' in names and state:
+                self.winCards = []
+                for i in range(5):
+                    self.winCards.append(str(int(state[0][2:]) + (i * int(state[0][:2]))))
+                    if len(self.winCards[i]) == 1:
+                        self.winCards[i] = '0' + self.winCards[i]
+                self.winner = name
+                return self.winner
             # WinCheck
             current_check = []
             for i in range(len(state)):
@@ -154,7 +166,7 @@ class GameScreen:
         if self.winCards:
             for i in range(len(self.winCards)):
                 if len(self.winCards[i]) == 1:
-                    self.winCards[i] = '0' + self.winCards[i]
+                    self.winCards[i] = '0' + str(self.winCards[i])
             for i in range(len(self.highlighter)):
                 if len(self.highlighter[i]) == 1:
                     self.highlighter[i] = '0' + self.highlighter[i]
@@ -187,8 +199,7 @@ class GameScreen:
                             pygame.draw.rect(self.win, (255, 255, 0),
                                              self.zoom(((43 + (j * 96)) - j // 5), ((81 + (i * 68)) - i // 2), 88, 65),
                                              border_radius=5)
-                    elif not (self.board[i][j] == 'xx') and not (
-                            self.board[i][j][2:] == 'AI' or self.board[i][j][2:] == 'US'):
+                    elif not (self.board[i][j] == 'xx') and len(self.board[i][j]) == 2:
                         pygame.draw.rect(self.win, (255, 255, 0),
                                          self.zoom(((43 + (j * 96)) - j // 5), ((81 + (i * 68)) - i // 2), 88, 65),
                                          border_radius=5)
@@ -198,25 +209,30 @@ class GameScreen:
         self.win.blit(self.BoardCards, self.zoom(37, 72))
         for i in range(10):
             for j in range(10):
-                if self.board[i][j][len(self.board[i][j]) - 2:] == 'US':
+                if self.board[i][j][-2:] == 'US':
                     pygame.draw.circle(self.win, (0, 0, 0), self.zoom((87 + (j * 96)), (110 + (i * 68))),
                                        int(27 // self.zr))
                     self.win.blit(self.USChip, self.zoom((62 + (j * 96)), (85 + (i * 68))))
-                elif self.board[i][j][len(self.board[i][j]) - 2:] == 'AI':
+                elif self.board[i][j][-2:] == 'AI':
                     pygame.draw.circle(self.win, (0, 0, 0), self.zoom((87 + (j * 96)), (110 + (i * 68))),
                                        int(27 // self.zr))
                     self.win.blit(self.AIChip, self.zoom((62 + (j * 96)), (85 + (i * 68))))
+                elif self.board[i][j][-2:] == 'GR':
+                    pygame.draw.circle(self.win, (0, 0, 0), self.zoom((87 + (j * 96)), (110 + (i * 68))),
+                                       int(27 // self.zr))
+                    self.win.blit(self.GRChip, self.zoom((62 + (j * 96)), (85 + (i * 68))))
         return return_txt
 
     def draw_cards(self) -> str:
         pos = pygame.mouse.get_pos()
         return_txt = ''
-        for i in range(7):
+        for i in range(len(self.cards)):
             if self.cards[i] == 'NO':
                 break
             if self.cards[i] == self.card_select:
-                pygame.draw.rect(self.win, (255, 255, 0), self.zoom((198 + (i * 100)), 768, 84, 116), border_radius=5)
-            card = pygame.Rect(self.zoom((200 + (i * 100)), 770, 80, 112))
+                pygame.draw.rect(self.win, (255, 255, 0), self.zoom(self.card_x[i] - 2, self.card_y[i] - 2,
+                                                                    84, 116), border_radius=5)
+            card = pygame.Rect(self.zoom(self.card_x[i], self.card_y[i], 80, 112))
             self.win.blit(self.cardIMGs[str(self.cards[i])], self.zoom(self.card_x[i], self.card_y[i]))
             if self.go and card.collidepoint(pos):
                 return_txt = 'USER' + str(i)
@@ -269,21 +285,27 @@ class GameScreen:
                     mini_board.append(render[0])
                     del render[0]
                 self.board.append(mini_board)
-            self.turn = bool(data0[1])
+            self.turn = True if data0[1] == '0' else False
             if self.card_change_stage != 1:
                 self.cards = data0[2].split(' ')
             self.top_txt = data0[3]
             self.reply = ''
-        else:
+        elif not self.player_names['r']:
             data0 = self.data.split('aaBaa')
-            self.BlueNames = data0[0::2]
-            self.RedNames = data0[1::2]
-            if self.name in self.BlueNames:
-                del self.BlueNames[self.BlueNames.index(self.name)]
-                self.BlueNames.insert(0, 'YOU')
-            elif self.name in self.RedNames:
-                del self.RedNames[self.RedNames.index(self.name)]
-                self.RedNames.insert(0, 'YOU')
+            if data0[-1] == '3':
+                self.player_names['b'] = ', '.join(data0[:-1][0::3])
+                self.player_names['r'] = ', '.join(data0[:-1][1::3])
+                self.player_names['g'] = ', '.join(data0[:-1][2::3])
+            elif data0[-1] == '2':
+                self.player_names['b'] = ', '.join(data0[:-1][0::2])
+                self.player_names['r'] = ', '.join(data0[:-1][1::2])
+            for key in self.player_names:
+                if self.name in self.player_names[key]:
+                    split = self.player_names[key].split(self.name)
+                    self.player_names[key] = 'YOU'.join(split)
+                    self.name = self.name.upper() + '\'S TURN'
+                    break
+            self.prepare_legend()
             self.top_txt = ''
 
     def top_text(self, text):
@@ -292,63 +314,90 @@ class GameScreen:
         self.win.blit(text, (x_val, int(25 // self.zr)))
 
     def card_positions(self):
+        card_len = len(self.cards)
         if self.card_change_stage == 0:
-            self.card_x = [200 + (i * 100) for i in range(7)]
-            self.card_y = [770 for _ in range(7)]
+            start = 550 - (int(card_len % 2) * 50) - (int(card_len / 2) * 100)
+            self.card_x = [start + (i * 100) for i in range(card_len)]
+            self.card_y = [770 for _ in range(card_len)]
         elif self.card_change_stage == 1:
-            self.card_x = [200 + (i * 100) for i in range(7)]
-            self.card_y = [770 for _ in range(7)]
+            start = 550 - (int(card_len % 2) * 50) - (int(card_len / 2) * 100)
+            self.card_x = [start + (i * 100) for i in range(card_len)]
+            self.card_y = [770 for _ in range(card_len)]
             self.card_y[self.card_remover] = int(770 + (self.card_change_count * 8.667))
             self.card_change_count += 1
             if self.card_change_count == 15:
                 self.card_change_count = 1
                 self.card_change_stage = 2
         elif self.card_change_stage == 2:
-            self.card_x = [200 + (i * 100) for i in range(self.card_remover)]
-            for i in range(self.card_remover, 7):
-                self.card_x.append(int(300 + (i * 100) - (self.card_change_count * 6.667)))
-            self.card_y = [770 for _ in range(6)]
+            start = 550 - (int(card_len % 2) * 50) - (int(card_len / 2) * 100)
+            self.card_x = [start + (i * 100) for i in range(self.card_remover)]
+            for i in range(self.card_remover + 1, card_len + 1):
+                self.card_x.append(int(start + (i * 100) - (self.card_change_count * 6.667)))
+            self.card_y = [770 for _ in range(card_len - 1)]
             self.card_y.append(920)
             self.card_change_count += 1
             if self.card_change_count == 15:
                 self.card_change_count = 1
                 self.card_change_stage = 3
         elif self.card_change_stage == 3:
-            self.card_x = [200 + (i * 100) for i in range(7)]
-            self.card_y = [770 for _ in range(6)]
+            start = 550 - (int(card_len % 2) * 50) - (int(card_len / 2) * 100)
+            self.card_x = [start + (i * 100) for i in range(card_len)]
+            self.card_y = [770 for _ in range(card_len - 1)]
             self.card_y.append(int(900 - (self.card_change_count * 8.667)))
             self.card_change_count += 1
             if self.card_change_count == 15:
                 self.card_change_count = 1
                 self.card_change_stage = 0
         elif self.card_change_stage == 4:
-            self.card_x = [200 + (i * 100) for i in range(7)]
-            self.card_y = [int(770 + (self.card_change_count * 8.667)) for _ in range(7)]
+            start = 550 - (int(card_len % 2) * 50) - (int(card_len / 2) * 100)
+            self.card_x = [start + (i * 100) for i in range(card_len)]
+            self.card_y = [int(770 + (self.card_change_count * 8.667)) for _ in range(card_len)]
             self.card_change_count += 1
             if self.card_change_count >= 18:
                 self.ending = False
                 self.card_change_stage = 5
 
-    def render_legend(self):
-        text1 = self.mini_f.render(self.BlueNames[0], True, (255, 255, 255))
-        text2 = self.mini_f.render(self.RedNames[0], True, (255, 255, 255))
-        if text1.get_width() > text2.get_width():
-            width = text1.get_width() + 25
+    def prepare_legend(self):
+        # txt1, txt2, txt3, y1, y2, y3, box_y, box_h, box_w
+        # self.player_names['g'] = 'testtest'
+        self.legend_prep = [self.mini_f.render(self.player_names['b'], True, (255, 255, 255)),
+                            self.mini_f.render(self.player_names['r'], True, (255, 255, 255)),
+                            self.mini_f.render(self.player_names['g'], True, (255, 255, 255))]
+        if self.player_names['g']:
+            self.legend_prep.extend((13, 28, 43, 6, 53))
+            if self.legend_prep[0].get_width() > self.legend_prep[1].get_width():
+                if self.legend_prep[0].get_width() > self.legend_prep[2].get_width():
+                    self.legend_prep.append(self.legend_prep[0].get_width() + 25)
+                else:
+                    self.legend_prep.append(self.legend_prep[2].get_width() + 25)
+            else:
+                if self.legend_prep[2].get_width() > self.legend_prep[1].get_width():
+                    self.legend_prep.append(self.legend_prep[2].get_width() + 25)
+                else:
+                    self.legend_prep.append(self.legend_prep[1].get_width() + 25)
         else:
-            width = text2.get_width() + 25
-        pygame.draw.rect(self.win, (251, 180, 41), self.zoom(34, 15, width + 2, 40), border_radius=5)
-        pygame.draw.rect(self.win, (15, 112, 25), self.zoom(35, 16, width, 38), border_radius=5)
-        self.win.blit(self.mini_USChip, self.zoom(37, 19))
-        self.win.blit(self.mini_AIChip, self.zoom(37, 35))
-        self.win.blit(text1, self.zoom(55, 23))
-        self.win.blit(text2, self.zoom(55, 38))
+            self.legend_prep.extend((23, 38, -50, 16, 38))
+            if self.legend_prep[0].get_width() > self.legend_prep[1].get_width():
+                self.legend_prep.append(self.legend_prep[0].get_width() + 25)
+            else:
+                self.legend_prep.append(self.legend_prep[1].get_width() + 25)
+
+    def render_legend(self):
+        pygame.draw.rect(self.win, (251, 180, 41), self.zoom(34, self.legend_prep[6] - 1, self.legend_prep[8] + 2,
+                                                             self.legend_prep[7] + 2), border_radius=5)
+        pygame.draw.rect(self.win, (15, 112, 25), self.zoom(35, self.legend_prep[6], self.legend_prep[8],
+                                                            self.legend_prep[7]), border_radius=5)
+        chips = [self.mini_USChip, self.mini_AIChip, self.mini_GRChip]
+        for i in range(3):
+            self.win.blit(self.legend_prep[i], self.zoom(55, self.legend_prep[i + 3]))
+            self.win.blit(chips[i], self.zoom(37, self.legend_prep[i + 3] - 4))
 
     def main_loop(self, go):
         self.go = go
         if self.gameEnded:
             self.card_positions()
             self.draw_cards()
-            if not self.turn:
+            if self.name == self.winner:
                 self.top_text('YOU WON')
             else:
                 self.top_text('YOU LOST')
@@ -362,7 +411,10 @@ class GameScreen:
             self.data = self.n.send(self.reply)
             self.update_data()
             self.card_positions()
-            self.top_text(self.top_txt)
+            if self.top_txt == self.name:
+                self.top_text('YOUR TURN')
+            else:
+                self.top_text(self.top_txt)
             self.render_legend()
             x = self.draw_board(self.go, self.card_select)
             self.clicked = self.draw_cards()
@@ -370,6 +422,16 @@ class GameScreen:
                 self.clicked = x
             if not self.turn:
                 self.reply = 'ping'
+                if self.is_five(self.board):
+                    if 'YOU' in self.player_names['b']:
+                        self.name = 'US'
+                    elif 'YOU' in self.player_names['r']:
+                        self.name = 'AI'
+                    elif 'YOU' in self.player_names['g']:
+                        self.name = 'GR'
+                    self.gameEnded = True
+                    del self.n
+                    self.card_change_stage = 4
             else:
                 self.detail = self.sequence_no
                 self.card_select = self.main(self.cards)
@@ -383,16 +445,27 @@ class GameScreen:
                             mini_board.append(render[0])
                             del render[0]
                         self.board.append(mini_board)
-                    self.turn = bool(temp[1])
                     if not (self.is_five(self.board)):
                         self.card_remover = self.cards.index(self.reply[2:])
                         self.card_change_stage = 1
                     else:
+                        if 'YOU' in self.player_names['b']:
+                            self.name = 'US'
+                        elif 'YOU' in self.player_names['r']:
+                            self.name = 'AI'
+                        elif 'YOU' in self.player_names['g']:
+                            self.name = 'GR'
                         self.gameEnded = True
                         del self.n
                         self.card_change_stage = 4
                     self.reply = 'ping'
                 elif self.is_five(self.board):
+                    if 'YOU' in self.player_names['b']:
+                        self.name = 'US'
+                    elif 'YOU' in self.player_names['r']:
+                        self.name = 'AI'
+                    elif 'YOU' in self.player_names['g']:
+                        self.name = 'GR'
                     self.gameEnded = True
                     del self.n
                     self.card_change_stage = 4
